ee12ae503a5c1ee51da107b7a5e18beab79d1830
Joel Garza <joelgarzatx>
2014-11-09 21:36:17 -0600

    Create item.php
    HTML table for item details

---
+++ code/item.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999" lang="en" xml:lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>My Store</title>
+</head>
+<body> 
+    <p> Items detail: </p>
+    <table>
+      <tr><td>Item ID</td><td></td></tr>
+      <tr><td>Category<</td><td></td></tr>
+      <tr><td>Title</td><td></td></tr>
+      <tr><td>Description</td><td></td></tr>
+      <tr><td>Price</td><td></td></tr>
+      <tr><td>Qty Available</td><td></td></tr>
+      <tr><td>Picture</td><td></td></tr>
+    </table>
+
+    <p><a href="catalog.php">Continue Shopping</a></p>
+</body>
+</html>
