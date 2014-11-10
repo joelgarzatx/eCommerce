a76d5379b9da3d557637a0885e1b385e4c75b87a
Joel Garza <git@joelgarzatx.com>
2014-11-09 21:24:36 -0600

    welcome and link
    Add text and link to catalog

--- code/index.php
+++ code/index.php
@@ -1,1 +1,11 @@
-<!-- File placeholder -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999" lang="en" xml:lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>My Store</title>
+</head>
+<body> 
+    <p> Welcome to my store. </p>
+    <p> <a href="catalog.php">Shop Now!</p>
+</body>
+</html>
