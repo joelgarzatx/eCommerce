90d933044651f30654aa988e3c0dacf1817836cc
Joel Garza <joel.garza@gmail.com>
2014-11-09 21:29:27 -0600

    Create cart.php
    HTML for cart table and link to catalog page

---
+++ code/cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999" lang="en" xml:lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>My Store</title>
+</head>
+<body> 
+    <p> Items in your cart: </p>
+    <table>
+      <tr>
+        <th>Item ID</th><th>Category</th><th>Title</th><th>Item Price</th><th>Qty</th><th>Total Price</th>
+      </tr>
+    </table>
+    
+    <p><a href="catalog.php">Continue Shopping</a></p>
+</body>
+</html>
