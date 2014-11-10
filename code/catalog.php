a5ca114c7f9fee209b67dc67a697559efbad0270
Joel Garza <git@joelgarzatx.com>
2014-11-09 21:24:59 -0600

    html for catalog table

---
+++ code/catalog.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999" lang="en" xml:lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>My Store</title>
+</head>
+<body> 
+    <p> Items available: </p>
+    <table>
+      <tr>
+        <th>Item ID</th><th>Category</th><th>Title</th><th>Price</th><th>Qty Available</th>
+      </tr>
+    </table>
+</body>
+</html>
